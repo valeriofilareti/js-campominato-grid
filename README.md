ba394f2e6c7c50e8e8a97af05f912fda93497055
valeriofilareti <valerio.filareti@gmail.com>
2023-03-29 15:46:20 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Griglia Campo Minato
+===
+
+## Consegna
+L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*
+L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro
+ed emetto un messaggio in console con il numero della cella cliccata.
+*/
+
+const container = document.querySelector('.container');
+const genBtn = document.querySelector('button')
+
+genBtn.addEventListener('click', function(){
+  for (i = 0; i < 100; i++) {
+    container.innerHTML += `<div class="box"></div>`
+  }
+  genBtn.classList.add('hide')
+})
